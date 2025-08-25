8efc27a382bdafa8fba0f40b61596218d15ae62d
Abhishek Kr. Sharma <abhisheksharma12891@gmail.com>
2025-08-25 11:15:15 +0530

    mapping1

---
+++ app/utils/mappings.py
@@ -0,0 +1,13 @@
+def get_states():
+    # TODO: scrape/fetch states + IDs from Jagriti
+    return [
+        {"id": 1, "name": "KARNATAKA"},
+        {"id": 2, "name": "DELHI"},
+    ]
+
+def get_commissions(state_id: int):
+    # TODO: scrape/fetch commissions for state_id
+    return [
+        {"id": 101, "name": "Bangalore 1st & Rural Additional"},
+        {"id": 102, "name": "Mysore"},
+    ]
